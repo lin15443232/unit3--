30ec39ce6d5d4cb4257510f0f13c96106dd7953a
lin15443232 <alicelin1115@gmail.com>
2017-01-27 15:19:43 +0000

    Add files via upload

---
+++ p5circles/sketch.js
@@ -0,0 +1,327 @@
+function setup() {
+ createCanvas(460,800);
+
+}
+
+function draw() {
+background(160);
+
+
+if (mouseIsPressed) {
+ if (mouseButton == LEFT)
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(70, 80, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(180, 80, 60, 60);  
+ 
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(290, 80, 60, 60);
+  
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(400, 80, 60, 60);  
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(30, 190, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(140, 190, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(250, 190, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(360, 190, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(470, 190, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(70, 310, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(180, 310, 60, 60);
+
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(290, 310, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(400, 310, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(30, 420, 60, 60);
+
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(140, 420, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(250, 420, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(360, 420, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(470, 420, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(70, 540, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(180, 540, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(290, 540, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(30, 650, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(400, 540, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(140, 650, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(250, 650, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(360, 650, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(470, 650, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(70, 770, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(180, 770, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(290, 770, 60, 60);
+
+ellipseMode(CENTER);  
+fill(0);  
+ellipse(400, 770, 60, 60);
+
+}
+
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(50, 40, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(160, 40, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(270, 40, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(380, 40, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(10, 150, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(120, 150, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(230, 150, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(340, 150, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(450, 150, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(50, 270, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(160, 270, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(270, 270, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(380, 270, 60, 60); 
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(10, 380, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(120, 380, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(230, 380, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(340, 380, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(450, 380, 60, 60);
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(50,500, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(160, 500, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(270, 500, 60, 60);
+ 
+ 
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(380, 500, 60, 60); 
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(10, 610, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(120, 610, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(230, 610, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(340, 610, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(450, 610, 60, 60); 
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(50,730, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(160, 730, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(270, 730, 60, 60);
+
+
+color(255, 204, 0);  
+fill(255,255,0);
+noStroke();
+ellipse(380, 730, 60, 60); 
+
+
+}
